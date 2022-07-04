c1a8dd6b93e26694dd12e9ae462f2e1f1fe09970
rungene <rungenesamuel@gmail.com>
2022-07-04 20:20:18 +0300

    refactor: Adding fun initializeCourse() function.

--- app/src/main/java/com/rungene/android/notekeeper/DataManager.kt
+++ app/src/main/java/com/rungene/android/notekeeper/DataManager.kt
@@ -3,4 +3,19 @@ package com.rungene.android.notekeeper
 class DataManager {
     val courses = HashMap<String,CourseInfo>()
     val notes = ArrayList<NoteInfo>()
+
+    private fun initializeCourse(){
+        var course = CourseInfo("android_intent","Android Programing With Intents")
+        courses.set(course.courseId,course)
+
+         course = CourseInfo("android_sync","Android Sync Programing and services")
+        courses.set(course.courseId,course)
+
+         course = CourseInfo(title = "Java Fundamentals :Jave Language", courseId = "java_lang")
+        courses.set(course.courseId,course)
+
+         course = CourseInfo("java_core","Java Fundamentals : The Core platform")
+        courses.set(course.courseId,course)
+
+    }
 }

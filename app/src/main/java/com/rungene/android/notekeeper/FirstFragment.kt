00fda6737045dbb548f3f669045d15917c44635d
rungene <rungenesamuel@gmail.com>
2022-07-05 20:37:22 +0300

    refactor : Creating a new instance of data manager, array adapter, associate the adapter with the spinner.

--- app/src/main/java/com/rungene/android/notekeeper/FirstFragment.kt
+++ app/src/main/java/com/rungene/android/notekeeper/FirstFragment.kt
@@ -1,10 +1,13 @@
 package com.rungene.android.notekeeper
 
+import android.R
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.navigation.fragment.findNavController
 import com.rungene.android.notekeeper.databinding.FragmentFirstBinding
 
@@ -32,9 +35,20 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
+       /* binding.buttonFirst.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }*/
+
+        val dataManager = DataManager()
+        val adapterCourses = context?.let {
+            ArrayAdapter<CourseInfo>(
+                it,
+                R.layout.simple_spinner_dropdown_item,
+                dataManager.courses.values.toList())
         }
+
+      binding.spinnerCourses.adapter =adapterCourses
+
     }
 
     override fun onDestroyView() {

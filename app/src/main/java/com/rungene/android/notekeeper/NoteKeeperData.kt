b025e6abe02f771da7913c6d0ebc4cbd41f80560
rungene <rungenesamuel@gmail.com>
2022-07-12 20:17:45 +0300

    refactor : Annotate the data class with @Parcelize and have it implement the Parcelable interface.

--- app/src/main/java/com/rungene/android/notekeeper/NoteKeeperData.kt
+++ app/src/main/java/com/rungene/android/notekeeper/NoteKeeperData.kt
@@ -1,9 +1,12 @@
 package com.rungene.android.notekeeper
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
 data class CourseInfo(val courseId:String,val title:String?=null) {
     override fun toString(): String {
         return title.toString()
     }
 }
-
-data class NoteInfo(var course:String?,var title:String,var text:String)
+@Parcelize
+data class NoteInfo(var course:String?,var title:String,var text:String): Parcelable

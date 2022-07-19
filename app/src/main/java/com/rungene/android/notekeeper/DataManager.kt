8bd650043e6c70766c83c3987434c1fd68ecbdd2
rungene <rungenesamuel@gmail.com>
2022-07-19 04:56:47 +0300

    refactor : Removing DataManager call when adding the notes.

--- app/src/main/java/com/rungene/android/notekeeper/DataManager.kt
+++ app/src/main/java/com/rungene/android/notekeeper/DataManager.kt
@@ -27,34 +27,35 @@ object DataManager {
            var course = courses["android_intent"].toString()
             var note = NoteInfo(course, "Dynamic intent resolution",
                 "Wow, intents allow components to be resolved at runtime")
-            DataManager.notes.add(note)
+
+            notes.add(note)
             note = NoteInfo(course, "Delegating intents",
                 "PendingIntents are powerful; they delegate much more than just a component invocation")
-            DataManager.notes.add(note)
+             notes.add(note)
 
             course = courses["android_sync"].toString()
             note = NoteInfo(course, "Service default threads",
                 "Did you know that by default an Android Service will tie up the UI thread?")
-            DataManager.notes.add(note)
+            notes.add(note)
             note = NoteInfo(course, "Long running operations",
                 "Foreground Services can be tied to a notification icon")
-            DataManager.notes.add(note)
+            notes.add(note)
 
             course = courses["java_lang"].toString()
             note = NoteInfo(course, "Parameters",
                 "Leverage variable-length parameter lists")
-            DataManager.notes.add(note)
+            notes.add(note)
             note = NoteInfo(course, "Anonymous classes",
                 "Anonymous classes simplify implementing one-use types")
-            DataManager.notes.add(note)
+            notes.add(note)
 
             course = courses["java_core"].toString()
             note = NoteInfo(course, "Compiler options",
                 "The -jar option isn't compatible with with the -cp option")
-            DataManager.notes.add(note)
+            notes.add(note)
             note = NoteInfo(course, "Serialization",
                 "Remember to include SerialVersionUID to assure version compatibility")
-            DataManager.notes.add(note)
+            notes.add(note)
 
         }
     }
